12c38df391119d7a80f1c0e81680168d083655ea
Serdsj <lifannnnof@mail.ru>
2023-10-24 12:44:10 +0300

    first commit

---
+++ js/background.js
@@ -0,0 +1,18 @@
+document.querySelectorAll
+createUl(container, data)
+
+
+
+const images = [
+  'image-1',
+  'image-2',
+  'images-3'
+]
+
+const bacgroundImageContainer = document.querySelector('.wrapper')
+
+if (wrapper) {
+  // wrapper.styles.opacity = 0;
+  wrapper.styles.backgrounImage = createRandomImage(images);
+  // wrapper.css.opacity = 1;
+}
